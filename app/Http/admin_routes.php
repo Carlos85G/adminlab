05a82f7d959ddf201544de99c0f9374e4c2b8be9
Carlos González <carlos85g@gmail.com>
2017-03-06 20:54:37 -0600

    Generación de módulos para sección de Préstamos

--- app/Http/admin_routes.php
+++ app/Http/admin_routes.php
@@ -70,4 +70,8 @@ Route::group(['as' => $as, 'middleware' => ['auth', 'permission:ADMIN_PANEL']], 
 	Route::get(config('laraadmin.adminRoute') . '/backup_dt_ajax', 'LA\BackupsController@dtajax');
 	Route::post(config('laraadmin.adminRoute') . '/create_backup_ajax', 'LA\BackupsController@create_backup_ajax');
 	Route::get(config('laraadmin.adminRoute') . '/downloadBackup/{id}', 'LA\BackupsController@downloadBackup');
+
+	/* ================== Prestamos ================== */
+	Route::resource(config('laraadmin.adminRoute') . '/prestamos', 'LA\PrestamosController');
+	Route::get(config('laraadmin.adminRoute') . '/prestamo_dt_ajax', 'LA\PrestamosController@dtajax');
 });
