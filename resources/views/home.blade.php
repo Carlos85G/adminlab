72195e01f30701508fde9a3424a8830349379553
¨Rogelio <rjmultimedia@gmail.com>
2017-03-06 23:15:44 -0600

    Cabios en home y calendario contacto no jala

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,22 +9,22 @@
     <meta property="og:title" content="{{ LAConfigs::getByKey('sitename') }}" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="{{ LAConfigs::getByKey('site_description') }}" />
-    
+
     <meta property="og:url" content="http://laraadmin.com/" />
     <meta property="og:sitename" content="laraAdmin" />
 	<meta property="og:image" content="http://demo.adminlte.acacha.org/img/LaraAdmin-600x600.jpg" />
-    
+
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@laraadmin" />
     <meta name="twitter:creator" content="@laraadmin" />
-    
+
     <title>{{ LAConfigs::getByKey('sitename') }}</title>
-    
+
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('/la-assets/css/bootstrap.css') }}" rel="stylesheet">
 
 	<link href="{{ asset('la-assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
-    
+
     <!-- Custom styles for this template -->
     <link href="{{ asset('/la-assets/css/main.css') }}" rel="stylesheet">
 
@@ -52,9 +52,9 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#home" class="smoothScroll">Home</a></li>
-                <li><a href="#about" class="smoothScroll">About</a></li>
-                <li><a href="#contact" class="smoothScroll">Contact</a></li>
+                <li class="active"><a href="#home" class="smoothScroll">Accede</a></li>
+                <!-- <li><a href="#about" class="smoothScroll">Avisos</a></li> -->
+                <li><a href="#contact" class="smoothScroll">Contáctanos</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 @if (Auth::guest())
@@ -73,24 +73,23 @@
 <div id="headerwrap">
     <div class="container">
         <div class="row centered">
-            <div class="col-lg-12">
-                <h1>{{ LAConfigs::getByKey('sitename_part1') }} <b><a>{{ LAConfigs::getByKey('sitename_part2') }}</a></b></h1>
-                <h3>{{ LAConfigs::getByKey('site_description') }}</h3>
-                <h3><a href="{{ url('/login') }}" class="btn btn-lg btn-success">Get Started!</a></h3><br>
-            </div>
             <div class="col-lg-2">
-                <h5>Amazing Functionalities</h5>
-                <p>for Modern Admin Panels</p>
+                <h5>Ya Reservaste tu Lab </h5>
+                <p>Acceso para Laboratorios</p>
                 <img class="hidden-xs hidden-sm hidden-md" src="{{ asset('/la-assets/img/arrow1.png') }}">
             </div>
             <div class="col-lg-8">
-                <img class="img-responsive" src="{{ asset('/la-assets/img/app-bg.png') }}" alt="">
+                <iframe src="https://calendar.google.com/calendar/embed?src=5hcsj2u0h68bir3qn00tus8u38%40group.calendar.google.com&ctz=America/Mexico_City" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe></div>
+            <div class="col-lg-12">
+                <h1>{{ LAConfigs::getByKey('sitename_part1') }} <b><a>{{ LAConfigs::getByKey('sitename_part2') }}</a></b></h1>
+                <h3>{{ LAConfigs::getByKey('site_description') }}</h3>
+                <h3><a href="{{ url('/login') }}" class="btn btn-lg btn-success">Inicia sesión</a></h3><br>
             </div>
             <div class="col-lg-2">
-                <br>
+                <!-- <br>
                 <img class="hidden-xs hidden-sm hidden-md" src="{{ asset('/la-assets/img/arrow2.png') }}">
                 <h5>Completely Packaged...</h5>
-                <p>for Future expantion of Modules</p>
+                <p>for Future expantion of Modules</p> -->
             </div>
         </div>
     </div> <!--/ .container -->
@@ -101,33 +100,19 @@
 <!-- INTRO WRAP -->
 <div id="intro">
     <div class="container">
-        <div class="row centered">
-            <h1>An Architecture designed To Excel</h1>
-            <br>
-            <br>
-            <div class="col-lg-4">
-                <i class="fa fa-cubes" style="font-size:100px;height:110px;"></i>
-                <h3>Modular</h3>
-                <p>Making Data Management fast and enjoyable.</p>
-            </div>
-            <div class="col-lg-4">
-                <i class="fa fa-paper-plane" style="font-size:100px;height:110px;"></i>
-                <h3>Easy to Install</h3>
-                <p>With single installation command.</p>
-            </div>
-            <div class="col-lg-4">
-                <i class="fa fa-cubes" style="font-size:100px;height:110px;"></i>
-                <h3>Customizable</h3>
-                <p>Easy to Manipulation the flows.</p>
-            </div>
-        </div>
+      <div class="panel panel-default">
+<div class="panel-heading">Boletín de noticias</div>
+<div class="panel-body">
+  Información sobre llave tel: 322-800-05-21
+</div>
+</div>
         <br>
         <hr>
     </div> <!--/ .container -->
 </div><!--/ #introwrap -->
 
 <!-- FEATURES WRAP -->
-<div id="features">
+<!-- <div id="features">
     <div class="container">
         <div class="row">
             <div class="col-lg-5 centered">
@@ -135,7 +120,7 @@
             </div>
 
             <div class="col-lg-7">
-				<h3 class="feature-title">What is LaraAdmin ?</h3><br>
+				<h3 class="feature-title">Que es </h3><br>
 				<ol class="features">
 					<li><strong>CMS</strong> (Content Management System) &#8211; Manages Modules &amp; their Data</li>
 					<li>Backend <strong>Admin Panel</strong> &#8211; Data can be used in front end applications with ease.</li>
@@ -154,43 +139,44 @@
             </div>
         </div>
     </div><!--/ .container -->
-</div><!--/ #features -->
+</div><!--/ #features --> -->
 
 <section id="contact" name="contact"></section>
 <div id="footerwrap">
     <div class="container">
         <div class="col-lg-5">
-            <h3>Contact Us</h3><br>
+            <h3>Contáctanos</h3><br>
             <p>
-				Dwij IT Solutions,<br/>
-				Web Development Company in Pune,<br/>
-                B4, Patang Plaza Phase 5,<br/>
-                Opp. PICT College,<br/>
-                Katraj, Pune, India - 411046
+				Maestria en Ingenieria de Software,<br/>
+				Desarrollado por <br/>
+                Ing. Rogelio Jiménez Meza<br/>
+                Ing. Carlos Eduardo González López<br/>
+                Ing. Jesus Adrian Arroyo Ceja
+              Centro Universitario de los Valles
             </p>
-			<div class="contact-link"><i class="fa fa-envelope-o"></i> <a href="mailto:hello@laraadmin.com">hello@laraadmin.com</a></div>
-			<div class="contact-link"><i class="fa fa-cube"></i> <a href="http://laraadmin.com">laraadmin.com</a></div>
-			<div class="contact-link"><i class="fa fa-building"></i> <a href="http://dwijitsolutions.com">dwijitsolutions.com</a></div>
+			<div class="contact-link"><i class="fa fa-envelope-o"></i> <a href="mailto:rogelio.jimenez@alumno.udg.mx">rogelio.jimenez@alumno.udg.mx</a></div>
+			<!-- <div class="contact-link"><i class="fa fa-cube"></i> <a href="http://laraadmin.com">laraadmin.com</a></div>
+			<div class="contact-link"><i class="fa fa-building"></i> <a href="http://dwijitsolutions.com">dwijitsolutions.com</a></div> -->
         </div>
 
         <div class="col-lg-7">
-            <h3>Drop Us A Line</h3>
+            <h3>Te gusto o tienes problemas con este sistema? Dejanos un mensaje</h3>
             <br>
             <form role="form" action="#" method="post" enctype="plain">
                 <div class="form-group">
-                    <label for="name1">Your Name</label>
+                    <label for="name1">Nombre</label>
                     <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
                 </div>
                 <div class="form-group">
-                    <label for="email1">Email address</label>
+                    <label for="email1">Correo</label>
                     <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
                 </div>
                 <div class="form-group">
-                    <label>Your Text</label>
+                    <label>Mensaje</label>
                     <textarea class="form-control" name="Message" rows="3"></textarea>
                 </div>
                 <br>
-                <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+                <button type="submit" class="btn btn-large btn-success">Enviar</button>
             </form>
         </div>
     </div>
@@ -198,7 +184,7 @@
 <div id="c">
     <div class="container">
         <p>
-            <strong>Copyright &copy; 2016. Powered by <a href="https://dwijitsolutions.com"><b>Dwij IT Solutions</b></a>
+            <strong>Copyright &copy; 2017. Powered by <a href="MISCUVALLES.com"><b>Maestria en Ingenieria de Software</b></a>
         </p>
     </div>
 </div>
