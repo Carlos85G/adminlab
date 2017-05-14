f8a80af806cca771ff1dd66d98956c1a27b18772
¨Rogelio <rjmultimedia@gmail.com>
2017-05-14 18:52:21 -0500

    Auto stash before cherrypick of "Limpieza de vistas"

--- resources/views/la/users/show.blade.php
+++ resources/views/la/users/show.blade.php
@@ -47,7 +47,7 @@
 			</div>
 			-->
 			<div class="dats1 pb">
-				<div class="clearfix">
+				<!-- <div class="clearfix">
 					<span class="pull-left">Task #1</span>
 					<small class="pull-right">20%</small>
 				</div>
@@ -55,10 +55,10 @@
 					<div class="progress-bar progress-bar-warning progress-bar-striped" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
 						<span class="sr-only">20% Complete</span>
 					</div>
-				</div>
+				</div> -->
 			</div>
 			<div class="dats1 pb">
-				<div class="clearfix">
+				<!-- <div class="clearfix">
 					<span class="pull-left">Task #2</span>
 					<small class="pull-right">90%</small>
 				</div>
@@ -66,17 +66,17 @@
 					<div class="progress-bar progress-bar-warning progress-bar-striped" style="width: 90%" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
 						<span class="sr-only">90% Complete</span>
 					</div>
-				</div>
+				</div> -->
 			</div>
 			<div class="dats1 pb">
-				<div class="clearfix">
+				<!-- <div class="clearfix">
 					<span class="pull-left">Task #3</span>
 					<small class="pull-right">60%</small>
-				</div>
+				</div> -->
 				<div class="progress progress-xs active">
-					<div class="progress-bar progress-bar-warning progress-bar-striped" style="width: 60%" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+					<!-- <div class="progress-bar progress-bar-warning progress-bar-striped" style="width: 60%" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
 						<span class="sr-only">60% Complete</span>
-					</div>
+					</div> -->
 				</div>
 			</div>
 		</div>
